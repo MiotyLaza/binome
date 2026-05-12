ef74ad726cd4bcfd12830b5ea2fae9c60472320b
RANAIVOSON ANDRIATAHINA Jonathan Fifaliana <jonathanranaivoson8@gmail.com>
2026-05-12 09:33:07 +0300

    Ito le config le database fotsiny io

--- sqlite/app/Config/Database.php
+++ sqlite/app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => '',
         'username'     => '',
         'password'     => '',
-        'database'     => '',
+        'database'     => WRITEPATH .'fitspace.db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
