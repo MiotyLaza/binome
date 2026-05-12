08516733ceaff6ff59a67be699b048abe9602130
MiotyLaza <miotyrakoto@gmail.com>
2026-05-12 10:31:06 +0300

    update

--- sqlite/app/Config/Database.php
+++ sqlite/app/Config/Database.php
@@ -30,7 +30,7 @@ class Database extends Config
         'username'     => '',
         'password'     => '',
         'database'     => WRITEPATH .'fitspace.db',
-        'DBDriver'     => 'SQL ite3',
+        'DBDriver'     => 'SQLite3',
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
